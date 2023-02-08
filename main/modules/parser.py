2235c6b966a0ea964a85a3f905f4d45fa1c21f8a
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-02-08 17:41:38 +0530

    Update parser.py

--- main/modules/parser.py
+++ main/modules/parser.py
@@ -65,6 +65,7 @@ async def auto_parser():
         try:
             await status.edit(await status_text("Idle..."),reply_markup=button1)
             await update_schedule()
+            await asyncio.sleep(6)
             await update_schedulex()
         except:
             pass
