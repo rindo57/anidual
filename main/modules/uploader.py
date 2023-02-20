abbb037d89124e8acf1da2145c924e919e0818aa
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-02-21 00:42:05 +0530

    Update uploader.py

--- main/modules/uploader.py
+++ main/modules/uploader.py
@@ -102,7 +102,7 @@ async def upload_video(msg: Message,file,id,tit,name,ttl):
 
             ) 
         files = {'file': open(file, 'rb')}
-        nanix = await x.edit(gcaption + "\n" "━━━━━━━━━━━━━━━━━━━" + "\n" + "Generating Link**", parse_mode = "markdown")
+        nanix = await x.edit(gcaption + "\n" "━━━━━━━━━━━━━━━━━━━" + "\n" + "Generating Link", parse_mode = "markdown")
         callapi = requests.post("https://api.filechan.org/upload", files=files)
         text = callapi.json()
         long_url = text['data']['file']['url']['full']
