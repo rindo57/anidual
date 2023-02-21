6c3c0ac6449d5914e147b383a2a1ac25d4b39087
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-02-21 21:14:49 +0530

    Update tg_handler.py

--- main/modules/tg_handler.py
+++ main/modules/tg_handler.py
@@ -8,7 +8,7 @@ from main.modules.utils import episode_linker, get_duration, get_epnum, status_t
 
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
-from main.modules.uploader import upload_video, thumbnail
+from main.modules.uploader import upload_video
 
 import os
 
