0afdc79746cf25c66a1765b7fbb906176d7f77e2
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-03-22 22:27:58 +0530

    Update uploader.py

--- main/modules/uploader.py
+++ main/modules/uploader.py
@@ -147,7 +147,7 @@ async def upload_video(msg: Message,file,id,tit,name,ttl):
 {gcaption}
 ━━━━━━━━━━━━━━━━━━━
 **External Download Links**
-[Filechan]({nyaa_text})  |  [Gofile]({gofuk_text})  |  [KrakenFiles]({krfuk_text})"""
+[Gofile]({gofuk_text})  |  [KrakenFiles]({krfuk_text})"""
         daze = await x.edit(output, parse_mode = "markdown")
     except Exception:
        await app.send_message(message.chat.id, text="Something Went Wrong!")
