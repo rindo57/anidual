a167e53b73e2251943f5de68add50a112df7e20e
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-03-28 02:00:44 +0530

    Update uploader.py

--- main/modules/uploader.py
+++ main/modules/uploader.py
@@ -110,7 +110,7 @@ async def upload_video(msg: Message,file,id,tit,name,ttl):
         callapi = requests.post("https://api.filechan.org/upload", files=files)
         text = callapi.json()
         long_url = text['data']['file']['url']['full']
-        api_url = f"http://ouo.io/qs/jezWr0hG?s={long_url}"
+        api_url = f"https://flashlink.in/api?api=aafa2d36a38398631679a74769a071b2154e08e7&url={long_url}&format=text"
         result = requests.get(api_url)
         nai_text = result.text
         da_url = "https://da.gd/"
@@ -118,23 +118,25 @@ async def upload_video(msg: Message,file,id,tit,name,ttl):
         shorten_url = f"{da_url}shorten"
         response = requests.get(shorten_url, params={"url": url})
         nyaa_text = response.text.strip()                                     
+        await asyncio.sleep(6)
         server = requests.get(url="https://api.gofile.io/getServer").json()["data"]["server"]
         uploadxz = requests.post(url=f"https://{server}.gofile.io/uploadFile", files={"upload_file": open(fukpath, 'rb')}).json()
         directlink = uploadxz["data"]["downloadPage"]    
-        gotn_url = f"http://ouo.io/qs/jezWr0hG?s={directlink}"
+        gotn_url = f"https://flashlink.in/api?api=aafa2d36a38398631679a74769a071b2154e08e7&url={directlink}&format=text"
         gofinal = requests.get(gotn_url)
         go_text = gofinal.text
         gourl = go_text
         gofile_url = f"{da_url}shorten"
         goresponse = requests.get(gofile_url, params={"url": gourl})
         gofuk_text = goresponse.text.strip()
+        await asyncio.sleep(6)
         krakenapi = requests.get(url="https://krakenfiles.com/api/server/available").json()
         krakenxurl = krakenapi['data']['url']
         krakentoken = krakenapi['data']['serverAccessToken']
         params = {'serverAccessToken': krakentoken} 
         krakenupload = requests.post(krakenxurl, files={'file': open(fukpath, 'rb')}, data=params).json()
         krakenlink = krakenupload['data']['url']
-        krtn_url = f"http://ouo.io/qs/jezWr0hG?s={krakenlink}"
+        krtn_url = f"https://flashlink.in/api?api=aafa2d36a38398631679a74769a071b2154e08e7&url={krakenlink}&format=text"
         krfinal = requests.get(krtn_url)
         kr_text = krfinal.text
         krurl = kr_text
