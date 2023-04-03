65d28827fc3b71a4ad9feab22551c9072616e55e
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-04-03 17:55:18 +0530

    Update compressor.py

--- main/modules/compressor.py
+++ main/modules/compressor.py
@@ -21,7 +21,7 @@ async def gg():
 async def mediainfo(file):
     try:
         process = await asyncio.create_subprocess_shell(
-            f"mediainfo '''{file}''' --Output=HTML",
+            f"mediainfo '''"video.mkv"''' --Output=HTML",
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE,
         )
