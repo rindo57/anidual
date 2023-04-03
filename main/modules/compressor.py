66502a202308f630851a1b53295bfd47f3eaace5
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-04-03 10:28:30 +0530

    Update compressor.py

--- main/modules/compressor.py
+++ main/modules/compressor.py
@@ -1,4 +1,8 @@
 import asyncio
+import aiofiles
+import aiohttp
+from html_telegraph_poster import TelegraphPoster
+from pathlib import Path
 
 from main.modules.utils import get_progress_text 
 
@@ -13,6 +17,29 @@ import subprocess
 async def gg():
           cmd = '''ffmpeg -hide_banner -loglevel quiet -progress "progressaa.txt" -i "video.mkv" -filter_complex "[0:v]drawtext=fontfile=font.ttf:text='t.me/animxt':fontsize=25:fontcolor=ffffff:alpha='if(lt(t,0),0,if(lt(t,5),(t-0)/5,if(lt(t,15),1,if(lt(t,20),(5-(t-15))/5,0))))':x=w-text_w-15:y=15" -c:v libx265 -s 192x144 -pix_fmt yuv420p10le -preset medium -r 24000/1001 -crf 24.2 -x265-params profile=main444-10:deblock=-1,-1:no-sao:aq-mode=2:aq-strength=0.75:bframes=6:frame-threads=4 -c:a libopus -b:a 96k "out.mkv" -y''',
           subprocess.Popen(cmd,shell=True)
+
+async def mediainfo(fukpath):
+    try:
+        process = await asyncio.create_subprocess_shell(
+            f"mediainfo '''{fukpath}''' --Output=HTML",
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        stdout, stderr = await process.communicate()
+        outX = stdout.decode()
+        client = TelegraphPoster(use_api=True)
+        client.create_api_token("Mediainfo")
+        page = client.post(
+            title="Mediainfo",
+            author="ANIMXT",
+            author_url=f"https://t.me/animxt",
+            text=outX,
+        )
+        return page.get("url")
+    except Exception as error:
+        print(error)
+        return None
+
 async def compress_video(total_time, videox, name, guessname):
 
   try:
