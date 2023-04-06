231e550a48e9039a2f99b107a644e76c9b336d60
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-04-06 21:24:04 +0530

    Update tg_handler.py

--- main/modules/tg_handler.py
+++ main/modules/tg_handler.py
@@ -100,6 +100,7 @@ async def start_uploading(data):
         title = data["title"]
         title = title.replace("Shinka", "Shin Shinka")
         title = title.replace("Ijiranaide, Nagatoro-san S2", "Ijiranaide, Nagatoro-san 2")
+        title = title.replace("Dr. Stone S3", "Dr Stone New World")
         link = data["link"]
 
         size = data["size"]
