5fa24262bd540383b321520db368981e71e207db
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-04-17 22:30:20 +0530

    Update uploader.py

--- main/modules/uploader.py
+++ main/modules/uploader.py
@@ -112,9 +112,9 @@ async def upload_video(msg: Message,file,id,tit,name,ttl):
             string = f"get-{converted_id}"
             base64_string = await encode(string)
             link = f"https://t.me/zoroloverbot?start={base64_string}"
-            await app.send_message(linkx_id,text={link})
+            await app.send_message(kayo_id,text={link})
     except Exception:
-            await app.send_message(message.chat.id, text="Something Went Wrong!")
+            await app.send_message(kayo_id, text="Something Went Wrong!")
     try:
 
             await r.delete()
