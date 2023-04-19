05e58c00ac90a438bb446a5c71ea72c315af3497
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-04-19 22:00:08 +0530

    Update utils.py

--- main/modules/utils.py
+++ main/modules/utils.py
@@ -1,5 +1,6 @@
 import re
 import asyncio
+from pyrogram import Client
 from math import floor
 import os
 from main import queue
@@ -32,8 +33,19 @@ def get_duration(file):
 
 FORWARD_AS_COPY = "True"
 
+async def reply_forward(message: Message, file_id: int):
+    try:
+        await message.reply_text(
+            f"**Here is Sharable Link of this file:**\n"
+            f"https://t.me/zoroloverobot?start=animxt_{str_to_b64(str(file_id))}\n\n"
+            f"__To Retrive the Stored File, just open the link!__",
+            disable_web_page_preview=True, quote=True)
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
+        await reply_forward(message, file_id)
+        
 ky_id = -1001948444792
-async def media_forward(bot, user_id: int, file_id: int):
+async def media_forward(bot: Client, user_id: int, file_id: int):
     try:
         if FORWARD_AS_COPY is True:
             return await bot.copy_message(chat_id=user_id, from_chat_id=ky_id,
@@ -45,8 +57,9 @@ async def media_forward(bot, user_id: int, file_id: int):
         await asyncio.sleep(e.value)
         return media_forward(bot, user_id, file_id)
     
-async def send_media_and_reply(bot, user_id: int, file_id: int):
+async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
+    await reply_forward(message=sent_message, file_id=file_id)
     await asyncio.sleep(2)
 
 
