e9b27102742a31f465face5473c7d22ed3349b1c
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-04-23 19:11:03 +0530

    Update tg_handler.py

--- main/modules/tg_handler.py
+++ main/modules/tg_handler.py
@@ -214,7 +214,7 @@ async def start_uploading(data):
         print("Uploading --> ",name)
 
         await status.edit(await status_text(f"Uploading {name }"),reply_markup=button1)
-        video = await upload_video(msg,fpath,id,tit,name,size,sourcetext,untext,subtitle) 
+        video = await upload_video(msg,fpath,id,tit,name,size,sourcetext,untext,subtitle,nyaasize) 
         try:
 
             os.remove("video.mkv")
