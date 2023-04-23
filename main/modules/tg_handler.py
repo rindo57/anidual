ab300d1e5549d71e852f2f95a3d5bc593e65d194
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-04-23 16:43:26 +0530

    Update tg_handler.py

--- main/modules/tg_handler.py
+++ main/modules/tg_handler.py
@@ -112,7 +112,7 @@ async def start_uploading(data):
         DATABASE_ID = -1001903052236
         name = name.replace(f" @animxt.","").replace(ext,"").strip()
         id, img, tit = await get_anime_img(get_anime_name(title))
-        msg = await app.send_photo(UPLOADS_ID,photo=img,caption=title)
+        msg = await app.send_photo(KAYO_ID,photo=img,caption=title)
         img, caption = await get_anilist_data(title)
 
         print("Downloading --> ",name)
