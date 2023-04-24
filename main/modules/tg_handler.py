6f6351c56dc46ec211aa770b0a7c79a5b33a882c
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-04-24 21:57:09 +0530

    Update tg_handler.py

--- main/modules/tg_handler.py
+++ main/modules/tg_handler.py
@@ -195,17 +195,6 @@ async def start_uploading(data):
 
             )   
         os.rename(file, fpath)
-        server = requests.get(url="https://api.gofile.io/getServer").json()["data"]["server"]
-        uploadxz = requests.post(url=f"https://{server}.gofile.io/uploadFile", files={"upload_file": open(fpath, 'rb')}).json()
-        directlink = uploadxz["data"]["downloadPage"]    
-        gotn_url = f"https://flashlink.in/api?api=aafa2d36a38398631679a74769a071b2154e08e7&url={directlink}&format=text"
-        gofinal = requests.get(gotn_url)
-        go_text = gofinal.text
-        gourl = go_text
-        da_url = "https://da.gd/"
-        gofile_url = f"{da_url}shorten"
-        goresponse = requests.get(gofile_url, params={"url": gourl})
-        gofuk_text = goresponse.text.strip()
         sourcefileid = str(videox.message_id)
         source_link = f"https://t.me/zoroloverobot?start=animxt_{str_to_b64(sourcefileid)}"
         com_id = int(main.message_id) + 1
@@ -233,13 +222,20 @@ async def start_uploading(data):
                 ],
             )
         
-        enrepl_markup=InlineKeyboardMarkup([InlineKeyboardButton(text="💬Comments",url=encomment)])
-        orgtext =  "**#Source_File**" + "\n" + f"**‣ File Name: `{filed}`**" + "\n" + "**‣ Video**: `1080p x264`" + "\n" + "**‣ Audio**: `Japanese`" + "\n" + f"**‣ Subtitle**: `{subtitle}`" + "\n" + f"**‣ File Size**: `{nyaasize}`" + "\n" + f"**‣ Duration**: {durationx}" + "\n" + f"**‣ Downloads**: [🔗Telegram File]({source_link}) [🔗Gofile]({gofuk_text})"
+        enrepl_markup=InlineKeyboardMarkup(
+                    [
+                        InlineKeyboardButton(
+                            text="💬Comments",
+                            url=encomment,
+                        ),
+                    ]
+            )
+        orgtext =  "**#Source_File**" + "\n" + f"**‣ File Name: `{filed}`**" + "\n" + "**‣ Video**: `1080p x264`" + "\n" + "**‣ Audio**: `Japanese`" + "\n" + f"**‣ Subtitle**: `{subtitle}`" + "\n" + f"**‣ File Size**: `{nyaasize}`" + "\n" + f"**‣ Duration**: {durationx}" + "\n" + f"**‣ Downloads**: [🔗Telegram File]({source_link}) 🔗Gofile)"
         await asyncio.sleep(5)
         unitext = await main.reply_text(orgtext, reply_markup=repl_markup)
         await asyncio.sleep(5)
         sourcetext =  f"**#Encoded_File**" + "\n" + f"**‣ File Name**: `{razo}`" + "\n" + "**‣ Video**: `720p HEVC x265 10Bit`" + "\n" + "**‣ Audio**: `Japanese`" + "\n" + f"**‣ Subtitle**: `{subtitle}`"
-        untext = await main.reply_text(sourcetext,  reply_markup=enrepl_markup)
+        untext = await main.reply_text(sourcetext, reply_markup=enrepl_markup)
         await asyncio.sleep(3)
         await app.send_sticker(KAYO_ID,"CAACAgUAAx0CXbNEVgABATemYrg6dYZGimb4zx9Q1DAAARzJ_M_NAAI6BQAC7s_BVQFFcU052MmMHgQ")
         os.rename(fpath,"video.mkv")
