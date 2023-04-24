f5134975243b51fed0616f6f327c9d837218f81c
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-04-25 02:04:56 +0530

    Update tg_handler.py

--- main/modules/tg_handler.py
+++ main/modules/tg_handler.py
@@ -237,10 +237,8 @@ async def start_uploading(data):
         unitext = await untextx.edit(orgtext, reply_markup=repl_markup)
         await asyncio.sleep(5)
         sourcetext =  f"**#Encoded_File**" + "\n" + f"**‣ File Name**: `{razo}`" + "\n" + "**‣ Video**: `720p HEVC x265 10Bit`" + "\n" + "**‣ Audio**: `Japanese`" + "\n" + f"**‣ Subtitle**: `{subtitle}`"
-        untexti = await main.reply_text(sourcetext)
+        untext = await main.reply_text(sourcetext)
         await asyncio.sleep(2)
-        untext = await untexti.edit(sourcetext)
-        await asyncio.sleep(3)
         await app.send_sticker(KAYO_ID,"CAACAgUAAxkBAAEU_9FkRrLoli952oqIMVFPftW12xYLRwACGgADQ3PJEsT69_t2KrvBLwQ")
         os.rename(fpath,"video.mkv")
         await asyncio.sleep(5)
