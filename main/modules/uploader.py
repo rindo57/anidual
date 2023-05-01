a53168178aa95e4c3a0de2bb2b8e6935003ed685
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-05-01 16:52:00 +0530

    Update uploader.py

--- main/modules/uploader.py
+++ main/modules/uploader.py
@@ -134,21 +134,3 @@ async def upload_video(msg: Message,file,id,tit,name,ttl,sourcetext,untext,subti
         pass
 
     return x.message_id
-
-@app.on_message(filters.command("start") & filters.private)
-async def start(bot, cmd: Message):
-    usr_cmd = cmd.text.split("_", 1)[-1]
-    kay_id = -1001642923224
-    if usr_cmd == "/start":
-       await cmd.reply_text("Bot seems online! ⚡️")
-    else:
-        try:
-            try:
-                file_id = int(b64_to_str(usr_cmd).split("_")[-1])
-            except (Error, UnicodeDecodeError):
-                file_id = int(usr_cmd.split("_")[-1])
-            GetMessage = await app.get_messages(kay_id, message_ids=file_id)
-            message_ids = GetMessage.message_id
-            await app.copy_message(chat_id=cmd.from_user.id, from_chat_id=kay_id, message_id=message_ids)
-        except Exception as err:
-            await cmd.reply_text(f"Something went wrong!\n\n**Error:** `XXXXXXX`")
