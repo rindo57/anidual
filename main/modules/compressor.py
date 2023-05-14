ea07514d8ed531d4219d3858704f71e5870379e3
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-05-14 22:58:00 +0530

    Update compressor.py

--- main/modules/compressor.py
+++ main/modules/compressor.py
@@ -15,7 +15,7 @@ import math
 import subprocess
 
 async def gg():
-          cmd = '''ffmpeg -hide_banner -loglevel quiet -progress "progressaa.txt" -i "video.mkv" -filter_complex "[0:v]drawtext=fontfile=font.ttf:text='t.me/animxt':fontsize=25:fontcolor=ffffff:alpha='if(lt(t,0),0,if(lt(t,5),(t-0)/5,if(lt(t,15),1,if(lt(t,20),(5-(t-15))/5,0))))':x=w-text_w-15:y=15" -c:v libx265 -s 1280x720 -pix_fmt yuv420p10le -preset medium -r 24000/1001 -crf 24 -x265-params deblock=1,1:aq-mode=2:aq-strength=0.85:frame-threads=4:bframes=6:no-info=1 -map 0:v -c:a libopus -b:a 80k -map 0:a -c:s copy -map 0:s? "out.mkv" -y''',
+          cmd = '''ffmpeg -hide_banner -loglevel quiet -progress "progressaa.txt" -i "video.mkv" -filter_complex "[0:v]drawtext=fontfile=font.ttf:text='t.me/animxt':fontsize=40:fontcolor=ffffff:alpha='if(lt(t,0),0,if(lt(t,5),(t-0)/5,if(lt(t,15),1,if(lt(t,20),(5-(t-15))/5,0))))':x=w-text_w-15:y=15" -c:v libx265 -s 1280x720 -pix_fmt yuv420p10le -preset medium -r 24000/1001 -crf 24 -x265-params deblock=1,1:aq-mode=2:aq-strength=0.85:frame-threads=4:bframes=6:no-info=1 -map 0:v -c:a libopus -b:a 96k -map 0:a -c:s copy -map 0:s? "out.mkv" -y''',
           subprocess.Popen(cmd,shell=True)
 
 async def compress_video(total_time,untext,name,sourcetext):
