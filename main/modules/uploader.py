2a969f9b9c7fcc2b38f2418f758ea44d5cc30077
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-05-27 16:56:27 +0530

    Update uploader.py

--- main/modules/uploader.py
+++ main/modules/uploader.py
@@ -122,7 +122,7 @@ async def upload_video(msg: Message,file,id,tit,name,ttl,sourcetext,untext,subti
             )
             encodetext =  f"{sourcetext}" "\n" + f"**‣ File Size**: `{size}`" + "\n" + f"**‣ Duration**: {durationx}" + "\n" + f"**‣ Downloads**: [🔗Telegram File]({teleshare}) [🔗Gofile]({gofuk_text})"
             await asyncio.sleep(5)
-            entext = await untext.edit(encodetext, reply_markup=repl_markup)
+            entext = await untext.edit(encodetext, disable_web_page_preview=True, reply_markup=repl_markup)
     except Exception:
             await app.send_message(kayo_id, text="Something Went Wrong!")
     try:
