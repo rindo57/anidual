b9bfe46ac7252f7504f00f3ae7251f50323882a3
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-06-01 23:56:45 +0530

    Update parser.py

--- main/modules/parser.py
+++ main/modules/parser.py
@@ -16,6 +16,7 @@ def trim_title(title: str):
     title = title.replace("Ousama Ranking - Yuuki no Takarabako", "Ousama Ranking Yuuki no Takarabako")
     title = title.replace(" Isekai wa Smartphone to Tomo ni. 2", " Isekai wa Smartphone to Tomo ni 2")
     title = title.replace("Kimetsu no Yaiba - Katanakaji no Sato Hen", "Demon Slayer Kimetsu No Yaiba To the Swordsmith Village")
+    title = title.replace("Stand My Heroes - Warmth of Memories - OVA", "Stand My Heroes Warmth of Memories - OVA")
     ext = ".mkv"
     title = title + ext
     return title
