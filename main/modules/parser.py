2d40fa90ddee43d90797a26f9d9e6d69a4dc60ec
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-06-16 19:48:18 +0530

    Update parser.py

--- main/modules/parser.py
+++ main/modules/parser.py
@@ -28,7 +28,7 @@ def multi_sub(title: str):
 def parse():
     a = feedparser.parse("https://siftrss.com/f/oyebWJBqN8")
     b = a["entries"]
-    b = b[0:11]
+    b = b[0:11:2]
     data = []    
 
     for i in b:
