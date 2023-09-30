74bb53ec3f6ad2ec78965c2dd55e16e3623bb9e2
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-10-01 00:46:33 +0530

    Update uploader.py

--- main/modules/uploader.py
+++ main/modules/uploader.py
@@ -107,7 +107,7 @@ async def upload_video(msg: Message,file,id,tit,name,ttl,sourcetext,untext,subti
 
             share_link = f"https://telegram.me/somayukibot?start=animxt_{str_to_b64(fid)}"            
             await asyncio.sleep(10)
-            id = is_fid_in_db(fid)
+            id = await is_fid_in_db(fid)
             if id:
                 hash = id["code"]
                 ddl = f"https://dxd.ownl.tk/dl/{hash}"
