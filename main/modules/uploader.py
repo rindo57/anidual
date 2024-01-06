1a9f7c3ca1c3e86ce1b161c2d9da970d63988221
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-01-06 21:04:58 +0530

    Update uploader.py

--- main/modules/uploader.py
+++ main/modules/uploader.py
@@ -109,11 +109,15 @@ async def upload_video(msg: Message,file,id,tit,name,ttl,sourcetext,untext,subti
             if id:
                 hash = id["code"]
                 ddl = f"https://ddl.animxt.fun/beta/{hash}"
-            
+            api_url = f"http://yoururl.in/api?api=41b0b500ae8a0ab78c9c6abefb9583530c2e0ec7&url={ddl}&format=text"
+            print(ddl)
+            result = requests.get(api_url)
+            nai_text = result.text
+   
+            url = nai_text
             shorten_url = f"{da_url}shorten"
-            response = requests.post(shorten_url, params={"url": ddl})
+            response = requests.post(shorten_url, params={"url": url})
             nyaa_text = response.text.strip()
-            print(nyaa_text)
             repl_markup=InlineKeyboardMarkup(
 
                 [
