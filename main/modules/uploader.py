351a1af76567e9e098e8a931ca63cbf2a8d875f4
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-04-10 18:20:09 +0530

    Update uploader.py

--- main/modules/uploader.py
+++ main/modules/uploader.py
@@ -42,57 +42,38 @@ from pyrogram.errors import FloodWait
 
 from main.inline import button1
 
-async def upload_video(msg: Message,file,id,tit,name,ttl,main,subtitle,nyaasize):
-
+async def upload_video(msg: Message, file, id, tit, name, ttl, main, subtitle, nyaasize):
     try:
-
         fuk = isfile(file)
-
         if fuk:
-
             r = msg
-
             c_time = time.time()
-
             duration = get_duration(file)
-
             durationx = get_durationx(file)
-
             size = get_filesize(file)
-
             ep_num = get_epnum(name)
-
             rest = tit
-            
             filed = os.path.basename(file)
             print('filed: ', filed)
             filed = filed.replace("[1080p Web-DL]", "[Web][720p x265 10Bit][Opus][Erai-raws]")
-
             fukpath = "downloads/" + filed
-
             caption = f"{filed}"
-
-
-            gcaption=f"`{filed}`**" + "\n" +  f"__({tit})__" + "\n" + "━━━━━━━━━━━━━━━━━━━" + "\n" + f"✓  **Subtitle**: `{subtitle}`"
-
+            gcaption = f"`{filed}`**" + "\n" + f"__({tit})__" + "\n" + "━━━━━━━━━━━━━━━━━━━" + "\n" + f"✓ **Subtitle**: `{subtitle}`"
             kayo_id = -1001373634390
-
             gay_id = 1159872623
             upid = int(main.id)
             print(upid)
             x = await app.edit_message_media(
-            chat_id=kayo_id,
-            message_id=upid,
-            InputMediaDocument(file),
-            file_name=filed
+                chat_id=kayo_id,
+                message_id=upid,
+                media=InputMediaDocument(file),
+                file_name=filed
             )
             await asyncio.sleep(3)
             await x.edit_caption(gcaption)
-
-
     except Exception:
+        await app.send_message(kayo_id, text="Something Went Wrong!")
 
-            await app.send_message(kayo_id, text="Something Went Wrong!")
 
     try:
         
