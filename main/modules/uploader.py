248fd5a9ab5e12095a1defdce67a276557136026
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-04-10 19:33:24 +0530

    Update uploader.py

--- main/modules/uploader.py
+++ main/modules/uploader.py
@@ -72,7 +72,7 @@ async def upload_video(msg: Message, file, id, tit, name, ttl, main, subtitle, n
             await asyncio.sleep(3)
             hash = "".join([random.choice(ascii_letters + digits) for n in range(50)])
             await save_file_in_db(filed, hash, upid)
-            gcaption = f"`{filed}: https://robot.anidlserverv1.me.in/beta/{hash}`" + "\n" + f"__({tit})__" + "\n" + "━━━━━━━━━━━━━━━━━━━" + "\n" + f"- **Subtitle**: `{subtitle}`"
+            gcaption = f"`{filed}: https://robot.anidlserverv1.me.in/beta/{hash}`" + "\n" + f"__({tit})__" + "\n" + "━━━━━━━━━━━━━━━━━━━━━━" + "\n" + f"- **Subtitle**: `{subtitle}`"
             dl_markup = InlineKeyboardMarkup(
                 [
                     [
