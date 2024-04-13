34c1d446b661c3d4cd6c967b71e101a40f355833
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-04-14 00:59:32 +0530

    Update uploader.py

--- main/modules/uploader.py
+++ main/modules/uploader.py
@@ -71,7 +71,7 @@ async def upload_video(msg: Message, img, file, id, tit, name, ttl, main, subtit
             )
             await asyncio.sleep(3)
             hash = "".join([random.choice(ascii_letters + digits) for n in range(50)])
-            save_file_in_db(filed, hash, upid)
+            save_file_in_db(filed, hash, subtitle, upid, img, audio_info)
             print(hash)
             gcaption = f"`📺 {filed}`\n\n`🔗 EP - {ep_num}:  https://robot.ddlserverv1.me.in/beta/{hash}`" + "\n\n" + f"🔠 __{tit}__" + "\n" + "\n" + f"📝 `{subtitle}`"
             dl_markup = InlineKeyboardMarkup(
