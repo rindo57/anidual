f78adb9070266bacf9934eb7e5aefef85bbfaa62
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-06-14 04:04:24 +0530

    Update parser.py

--- main/modules/parser.py
+++ main/modules/parser.py
@@ -17,6 +17,7 @@ def trim_title(title: str):
     title = title.replace(" (JA)", "")
     title = title.replace(" (Japanese Audio)", "")
     title = title.replace(" (Chinese Audio)", "")
+    title = title.replace(" (Multi) ", "")
     title = title.replace("Tian Guan Ci Fu Di Er Ji", "Heaven Official's Blessing S2")
     title = title.replace("(AAC 2.0) ", "")
     ext = ".mkv"
