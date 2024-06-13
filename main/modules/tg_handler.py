e01071f7a05a962873fddc9ab642a5359582a11b
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-06-14 01:06:26 +0530

    Update tg_handler.py

--- main/modules/tg_handler.py
+++ main/modules/tg_handler.py
@@ -109,7 +109,8 @@ async def start_uploading(data):
             dbtit = data["title"]
             link = data["link"]
             size = data["size"]
-            nyaasize = data["size"]
+            size = size.replace("GB", " GiB")
+            nyaasize = size
             subtitle = data["subtitle"]
             name, ext = title.split(".")
 
