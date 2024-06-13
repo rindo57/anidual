822bae55f1adfecf85c27b1cebdf2d8b540ee681
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-06-13 12:24:45 +0530

    Update parser.py

--- main/modules/parser.py
+++ main/modules/parser.py
@@ -37,7 +37,7 @@ def parse():
         item['subtitle'] = multi_sub(i['title'])
         item['size'] = i['nyaa_size']   
         item['link'] = "magnet:?xt=urn:btih:" + i['nyaa_infohash']
-        item['480p'] = 0
+        item['480p'] = '0'
         data.append(item)
         data.reverse()
     return data
