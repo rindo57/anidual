b644999ebafc41e0b9bae81b44a1e6d0e369f972
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-06-15 17:03:48 +0530

    Update db.py

--- main/modules/db.py
+++ main/modules/db.py
@@ -100,8 +100,9 @@ def save_1080p(name):
         upsert=True,
     )
     return
-def save_file_in_db(filed, hash, subtitle, img, audio_info, tit, alink, size, upid=None):
-    filesdb.update_one(
+
+async def save_file_in_db(filed, hash, subtitle, img, audio_info, tit, alink, size, upid=None):
+    await filesdb.update_one(
         {
             "hash": hash,
             "fid": str(upid),
