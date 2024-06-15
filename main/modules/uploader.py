72568178aaefa1146f2356a674abf786933aa5cc
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-06-15 17:04:51 +0530

    Update uploader.py

--- main/modules/uploader.py
+++ main/modules/uploader.py
@@ -79,7 +79,8 @@ async def upload_video(msg: Message, title, img, file, id, tit, name, ttl, main,
             )
             await asyncio.sleep(3)
             hash = "".join([random.choice(ascii_letters + digits) for n in range(50)])
-            save_file_in_db(filed, hash, subtitle, img, audio_info, tit, alink, size, upid)
+            print("hash1:", hash)
+            await save_file_in_db(filed, hash, subtitle, img, audio_info, tit, alink, size, upid)
             print(hash)
             ddlurl = f"https://anidl.ddlserverv1.me.in/beta/{hash}"
             gcaption = f"`📺 {filed}`\n\n`🔗 EP - {ep_num}:  https://anidl.ddlserverv1.me.in/beta/{hash}`" + "\n\n" + f"🔠 __{tit}__" + "\n" + "\n" + f"📝 `{subtitle}`"
