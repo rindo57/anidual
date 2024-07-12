1c3e0591fa155bc1333947f72db546425088da23
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-07-13 00:35:56 +0530

    Update parser.py

--- main/modules/parser.py
+++ main/modules/parser.py
@@ -25,8 +25,8 @@ def trim_title(title: str):
                 title = f"[AniDL] {at.strip()} - {episode.strip()} [Web ~ {source.strip()}]"
     title = title.replace("[YouDeer]", "[AniDL]")
     title = title.replace("(WEB 1080p Dual Audio) | My Deer Friend Nokotan", "[Web ~ YouDeer]")
-    print(title)
-    return title + ".mkv"
+    title = title +".mkv"
+    return title
 
 def multi_sub(title: str):
     subtitle = title.split()[-1] 
